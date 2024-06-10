c8fa69a49252964abe44753132a42f7b08e63ace
Abdurahman <131678206+ALP0MISH@users.noreply.github.com>
2024-06-10 17:16:12 +0600

    Add room and source (#10)
    Add room and source

---
+++ data/src/main/kotlin/com/example/general/day/data/di/DataBaseModule.kt
@@ -0,0 +1,22 @@
+package com.example.general.day.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.general.day.data.local.database.WeatherDataBase
+import dagger.Module
+import dagger.Provides
+
+private const val WEATHER_DATABASE = "weather_database"
+
+@Module
+class DataBaseModule {
+
+    @Provides
+    fun provideWeatherDatabase(context: Context): WeatherDataBase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            WeatherDataBase::class.java,
+            WEATHER_DATABASE
+        ).build()
+    }
+}
