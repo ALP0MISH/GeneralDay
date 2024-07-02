e365da8b8601e99d4d01dfae1ff08d1e2c47e226
Abdurahman <131678206+ALP0MISH@users.noreply.github.com>
2024-07-02 17:00:11 +0600

    Add core component (#24)

---
+++ core/src/main/kotlin/com/example/general/day/core/viewModel/component/DaggerViewModelFactory.kt
@@ -0,0 +1,23 @@
+package com.example.general.day.core.viewModel.component
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.internal.Provider
+import javax.inject.Inject
+
+class DaggerViewModelFactory @Inject constructor(
+    private val creators: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
+        return try {
+            @Suppress("UNCHECKED_CAST")
+            creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
