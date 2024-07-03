481cb3a4cc9e73cd189d3feea8a7cee9c45ddfa6
Alpomish <abdurahmangaziev6@gmail.com>
2024-07-03 15:12:24 +0600

    Add again color to ui-core

--- ui-core/src/main/kotlin/com/example/general/day/ui/core/theme/MaterialTheme.kt
+++ ui-core/src/main/kotlin/com/example/general/day/ui/core/theme/MaterialTheme.kt
@@ -17,14 +17,14 @@ import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    secondary = DarkGray,
+    tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    secondary = LightGray,
 )
 
 @Composable
