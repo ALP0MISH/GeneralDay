2f5405ea7249b34234795ce042f49029fa031fc2
Alpomish <abdurahmangaziev6@gmail.com>
2024-09-06 18:49:52 +0600

    Add checking the weather availability in room

--- feature-modules/favorite/favorite-impl/src/main/kotlin/com/example/general/day/favorite/impl/ui/FavoriteScreen.kt
+++ feature-modules/favorite/favorite-impl/src/main/kotlin/com/example/general/day/favorite/impl/ui/FavoriteScreen.kt
@@ -113,13 +113,15 @@ internal fun FavoriteScreen(
                     .align(Alignment.BottomCenter)
                     .background(
                         brush = Brush.verticalGradient(
-                            colors = listOf(Color.Transparent, Color.White),
+                            colors = listOf(
+                                Color.Transparent,
+                                MaterialTheme.colorScheme.background
+                            ),
                             startY = 0f,
                             endY = 300f
                         )
                     )
             )
-
             Row(
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
